473c3799427d940e10f55f59a233b22458bf8f5c
Yair-oliva <43019674+Yair-oliva@users.noreply.github.com>
2019-05-12 22:32:27 -0300

    first  commit

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+function mostrar_ocultar(id){
+    if(document.getElementById(id).style.display=='block'){
+        document.getElementById(id).style.display='none';
+    }
+    else{
+        document.getElementById(id).style.display='block';
+    }
+}
